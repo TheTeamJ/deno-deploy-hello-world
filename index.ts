f36bf65a12c0e851ec235a1b7614ffb86edac0a7
daiiz <da.iz.mg@gmail.com>
2021-04-01 23:01:19 +0900

    add doc link

--- index.ts
+++ index.ts
@@ -1,3 +1,4 @@
+// https://deno.com/deploy/docs/hello-world
 addEventListener("fetch", (event) => {
   const response = new Response("Hello World!!!!", {
     headers: { "content-type": "text/plain" },
